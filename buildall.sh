267fc321d2e206bfbaa5d9320d3c8fa0df183da7
autun12 <austindrane56@gmail.com>
2019-04-03 16:21:40 -0500

    finally remembered to add Unit1 and Unit2

--- buildall.sh
+++ buildall.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-#javac Unit3/src/*.java
+javac Unit1/src/chapterone/*.java
+javac Unit2/src/chaptertwo/*.java
 javac Unit4/Ch4Exercise4/src/michealdrane/*.java
 javac Unit4/Mini-Project/src/michealdrane/*.java
 javac Unit5/Ch5Exercises/src/michealdrane/*.java
 javac Unit6/Ch6Exercises/src/michealdrane/*.java
+javac Unit7/Ch7Exercises/src/michealdrane/*.java
+javac Unit8/Ch8Exercises/src/michealdrane/*.java
+javac Unit8/Mini-Project2/src/michealdrane/*.java
+javac Unit9/Ch9Exercises/src/michealdrane/*.java
+javac Unit10/Ch10Exercises/src/michealdrane/*.java
+javac Unit11/Ch12Exercises/src/michealdrane/*.java
